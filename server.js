48ba5ec0d93bc31ebe1f128138304878203a3482
Elbert Shan <shanelbert@yahoo.com>
2020-12-10 12:58:42 +0700

    Fungsi selesai

---
+++ server.js
@@ -0,0 +1,136 @@
+const { Pool } = require('pg');
+const express = require('express')
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 5000
+
+const pool = new Pool({
+	host: 'localhost',
+	user: 'postgres',
+	database: 'qm',
+	password: 'e',
+});
+
+// function isValid(req, requiredAttribute) {
+//   let valid;
+//   requiredAttribute.every((val) => {
+//     valid = ((req.query[val] !== undefined) && (req.query[val] !== ""));
+//     return valid;
+//   });
+// }
+
+express()
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.header(
+      "Access-Control-Allow-Origin",
+      "http://localhost:3000"
+    );
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE')
+    next();
+  })
+	.post('/pengantri', (req, res) => {
+    pool.connect().then((client) => {
+      return client.query("INSERT INTO pengantri (namawakil, jumlah, waktumulai) SELECT $1, $2, (current_time at time zone 'cxt')::TIME(0);", [req.body.namawakil, req.body.jumlah])
+        .then(() => {
+          client.release();
+          res.status(200).send('Pengantri berhasil ditambahkan');
+        }).catch((err) => {
+          client.release();
+          console.log(err.stack);
+          res.status(500).send("Pengantri gagal ditambahkan");
+        })
+    })
+	})
+  .get('/pengantri', (req, res) => {
+    pool.connect().then((client) => {
+        return client.query('SELECT * FROM pengantri')
+          .then((result) => {
+            client.release();
+            res.json(result.rows);
+          })
+          .catch(err => {
+            client.release();
+            console.log(err.stack);
+            res.status(500).send("Data pengantri gagal diperoleh");
+          })
+      })
+	})
+  .delete('/pengantri', (req, res) => {
+    pool.connect().then((client) => {
+      return client.query('DELETE FROM pengantri WHERE id=$1;', [req.query.id]).then(() => {
+        client.query('SELECT COUNT(*) FROM pengantri;').then((rescount) => {
+          if (rescount.rows[0].count === '0') {
+            client.query('ALTER SEQUENCE pengantri_id_seq RESTART WITH 1;').then(() => {
+              client.release();
+              res.status(200).send(`Pengantri ${req.query.id} berhasil dihapus`);
+            }).catch(err => {
+              throw err;
+            });
+          } else {
+            client.release();
+            res.status(200).send(`Pengantri ${req.query.id} berhasil dihapus`);
+          }
+        }).catch(err => {
+          throw err;
+        });
+      }).catch(err => {
+        client.release();
+        console.log(err.stack);
+        res.status(500).send(`Pengantri ${req.query.id} gagal dihapus`);
+      })
+    })
+  })
+	.get('/meja', (req, res) => {
+    pool.connect().then((client) => {
+      return client.query("SELECT * FROM meja")
+        .then((result) => {
+          client.release();
+          res.json(result.rows);
+        }).catch((err) => {
+          client.release();
+          console.log(err.stack);
+          res.status(500).send("Data meja gagal diperoleh");
+        })
+    })
+	})
+  .put('/meja/kosong', (req, res) => {
+    pool.connect().then((client) => {
+      return client.query("UPDATE meja SET status = false WHERE id = $2 AND kapasitas = $3", [req.query.id, req.query.kapasitas])
+        .then(() => {
+          client.release();
+          res.status(200).send(`Status meja ${req.query.id} berhasil diganti`);
+        }).catch((err) => {
+          client.release()
+          console.log(err.stack);
+          res.status(500).send("Status meja gagal diganti");
+        })
+    })
+  })
+  .put('/meja', (req, res) => {
+    let query = `UPDATE meja SET status = ${req.body.status} WHERE `;
+    if (req.body.meja.length === 0) {
+      res.status(200).send("Tidak ada meja yang statusnya diubah");
+    } else {
+      query += `(id = ${req.body.meja[0].id} AND kapasitas = ${req.body.meja[0].kapasitas})`
+      for (let i=1; i<=req.body.meja.length-1; i++) {
+        query += ` OR (id = ${req.body.meja[i].id} AND kapasitas = ${req.body.meja[i].kapasitas})`
+      }
+    }
+    pool.connect().then((client) => {
+      return client.query(query)
+        .then(() => {
+          client.release();
+          res.status(200).send(`Status meja berhasil diganti`);
+        }).catch((err) => {
+          client.release()
+          console.log(err.stack);
+          res.status(500).send("Status meja gagal diganti");
+        })
+    })
+  })
+	.listen(PORT, () => console.log(`Listening on ${PORT}`));
